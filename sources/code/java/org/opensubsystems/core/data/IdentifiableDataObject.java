811f4a2588434d5bf3b57b17e83c57521f719d05
bastafidli <bastafidli@opensubsystems.org>
2013-04-19 12:03:14 -0400

    Added setters.

--- sources/code/java/org/opensubsystems/core/data/IdentifiableDataObject.java
+++ sources/code/java/org/opensubsystems/core/data/IdentifiableDataObject.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2003 - 2012 OpenSubsystems.com/net/org and its owners. All rights reserved.
+ * Copyright (C) 2003 - 2013 OpenSubsystems.com/net/org and its owners. All rights reserved.
  * 
  * This file is part of OpenSubsystems.
  *
@@ -42,9 +42,27 @@ public interface IdentifiableDataObject
    String getName();
    
    /**
+    * Set name of the data object
+    * 
+    * @param strName - name of the generic data
+    */
+   void setName(
+      String strName
+   );
+   
+   /**
     * Get description of the data object
     * 
     * @return String - description of the generic data
     */
    String getDescription();
+   
+   /**
+    * Set description of the data object
+    * 
+    * @param strDescription - description of the generic data
+    */
+   void setDescription(
+      String strDescription
+   );
 }
