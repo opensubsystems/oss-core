d934d8f953483d1a9a9f86cdd9fd934c489652a7
miro <miro@DESKTOP>
2015-09-12 22:44:54 -0400

    Added more tests.

--- sources/tests/java/org/opensubsystems/core/data/DataDescriptorTest.java
+++ sources/tests/java/org/opensubsystems/core/data/DataDescriptorTest.java
@@ -19,6 +19,8 @@
 
 package org.opensubsystems.core.data;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.opensubsystems.core.data.TestDataObject.TestDataObjectDataDescriptor;
 import org.opensubsystems.core.util.test.OSSTestCase;
 
@@ -99,7 +101,66 @@ public class DataDescriptorTest extends OSSTestCase
 					  descriptor1 == descriptor2);      
    }
 
+   /**
+    * Test getDataType method 
+    * 
+    * @throws Exception - and error has occurred  
+    */
+   public void testGetDataTypeWithRemappedValues(
+   ) throws Exception
+   {
+		// Remap the desired data type to a new value
+		Map<Integer, Integer> mpRemmapedDataTypes = new HashMap<>(1);
+		DataDescriptorManager original;
 
+		original = DataDescriptorManager.getManagerInstance();
+		try
+		{
+			// We have to setup new manager instance since if some tests were already
+			// run and data objects were created the manager doesn't allow remapping
+			// With new instance we will circumvent the restriction
+			DataDescriptorManager.setManagerInstance(new DataDescriptorManager());
+			mpRemmapedDataTypes.put(
+				TestDataObject.TestDataObjectDataDescriptor.TEST_DATA_TYPE_DESIRED_VALUE,
+				TestDataObject.TestDataObjectDataDescriptor.TEST_DATA_TYPE_DESIRED_VALUE + 1);
+			DataDescriptorManager.getManagerInstance().setDesiredDataTypeMap(mpRemmapedDataTypes);
+
+         DataDescriptor descriptor1;
+      
+         // Data descriptors should be really accessed using DataDescriptorManager
+         // which properly initializes them
+         descriptor1 = DataDescriptorManager.getInstance(TestDataObjectDataDescriptor.class);
+
+         assertNotNull("DataDescriptorManager must always be able to create instance"
+                       + " of data descriptor", descriptor1);
+
+         // When properly initialized by DataDescriptorManager we should be able 
+         // to get the data type
+         assertEquals("Data type doesn't match",
+                      TestDataObject.TestDataObjectDataDescriptor.TEST_DATA_TYPE_DESIRED_VALUE + 1,
+                      descriptor1.getDataType());
+
+         // Another way how to register the descriptor with DataDescriptorManager
+         // is to create data type that uses this descriptor
+         TestDataObject data = new TestDataObject();
+         DataDescriptor descriptor2;
+
+         assertEquals("Data descriptor doesnt match the expected class",
+                      TestDataObject.TestDataObjectDataDescriptor.class,
+                      data.getDataDescriptorClass());
+
+         descriptor2 = data.getDataDescriptor();
+
+         // This also implies that the data type is of course the same
+         assertTrue("Data descriptor instance is shared between all instances of class",
+                    descriptor1 == descriptor2);      
+		}
+		finally
+		{
+			DataDescriptorManager.setManagerInstance(original);
+		}
+   }
+   
    /**
     * Test getDataTypeAsObject method 
     * 
@@ -123,4 +184,272 @@ public class DataDescriptorTest extends OSSTestCase
 		assertTrue("Data type object representation is shared between invocations",
 					  iFirst == iSecond);
 	}
+
+   /**
+    * Test getDesiredDataType method 
+    * 
+    * @throws Exception - and error has occurred  
+    */
+   public void testGetDesiredDataType(
+   ) throws Exception
+   {
+		DataDescriptor descriptor1;
+		
+      // First create simple instance of the data descriptor, since we should
+      // be able to get desired data type this way just fine
+      descriptor1 = new TestDataObjectDataDescriptor();
+      
+		assertEquals("Desired data type doesn't match",
+					    TestDataObject.TestDataObjectDataDescriptor.TEST_DATA_TYPE_DESIRED_VALUE,
+						 descriptor1.getDesiredDataType());
+
+      // Data descriptors should be really accessed using DataDescriptorManager
+      // which properly initializes them, we shouldbe able to still get desired 
+      // data type this way just fine
+      descriptor1 = DataDescriptorManager.getInstance(TestDataObjectDataDescriptor.class);
+      
+      assertNotNull("DataDescriptorManager must always be able to create instance"
+                    + " of data descriptor", descriptor1);
+      
+      // When properly initialized by DataDescriptorManager we should be able 
+      // to get the data type
+		assertEquals("Desired data type doesn't match",
+					    TestDataObject.TestDataObjectDataDescriptor.TEST_DATA_TYPE_DESIRED_VALUE,
+						 descriptor1.getDesiredDataType());
+      
+      // Another way how to register the descriptor with DataDescriptorManager
+      // is to create data type that uses this descriptor,  we should be able 
+      // to get the data type
+		TestDataObject data = new TestDataObject();
+		DataDescriptor descriptor2;
+
+		assertEquals("Data descriptor doesnt match the expected class",
+					    TestDataObject.TestDataObjectDataDescriptor.class,
+						 data.getDataDescriptorClass());
+      
+		descriptor2 = data.getDataDescriptor();
+      
+      // This also implies that the data type is of course the same
+		assertTrue("Data descriptor instance is shared between all instances of class",
+					  descriptor1 == descriptor2);      
+   }
+
+   /**
+    * Test getDesiredDataType method 
+    * 
+    * @throws Exception - and error has occurred  
+    */
+   public void testGetDesiredDataTypeWithRemappedValues(
+   ) throws Exception
+   {
+		// Remap the desired data type to a new value
+		Map<Integer, Integer> mpRemmapedDataTypes = new HashMap<>(1);
+		DataDescriptorManager original;
+
+		original = DataDescriptorManager.getManagerInstance();
+		try
+		{
+			// We have to setup new manager instance since if some tests were already
+			// run and data objects were created the manager doesn't allow remapping
+			// With new instance we will circumvent the restriction
+			DataDescriptorManager.setManagerInstance(new DataDescriptorManager());
+			mpRemmapedDataTypes.put(
+				TestDataObject.TestDataObjectDataDescriptor.TEST_DATA_TYPE_DESIRED_VALUE,
+				TestDataObject.TestDataObjectDataDescriptor.TEST_DATA_TYPE_DESIRED_VALUE + 1);
+			DataDescriptorManager.getManagerInstance().setDesiredDataTypeMap(mpRemmapedDataTypes);
+
+         DataDescriptor descriptor1;
+      
+         // Data descriptors should be really accessed using DataDescriptorManager
+         // which properly initializes them
+         descriptor1 = DataDescriptorManager.getInstance(TestDataObjectDataDescriptor.class);
+
+         assertNotNull("DataDescriptorManager must always be able to create instance"
+                       + " of data descriptor", descriptor1);
+
+         // When properly initialized by DataDescriptorManager we should be able 
+         // to get the desirec data type as the original value
+         assertEquals("Desired data type doesn't match",
+                      TestDataObject.TestDataObjectDataDescriptor.TEST_DATA_TYPE_DESIRED_VALUE,
+                      descriptor1.getDesiredDataType());
+
+         // Another way how to register the descriptor with DataDescriptorManager
+         // is to create data type that uses this descriptor
+         TestDataObject data = new TestDataObject();
+         DataDescriptor descriptor2;
+
+         assertEquals("Data descriptor doesnt match the expected class",
+                      TestDataObject.TestDataObjectDataDescriptor.class,
+                      data.getDataDescriptorClass());
+
+         descriptor2 = data.getDataDescriptor();
+
+         // This also implies that the data type is of course the same
+         assertTrue("Data descriptor instance is shared between all instances of class",
+                    descriptor1 == descriptor2);      
+		}
+		finally
+		{
+			DataDescriptorManager.setManagerInstance(original);
+		}
+   }
+
+   /**
+    * Test getDisplayableViewName method 
+    * 
+    * @throws Exception - and error has occurred  
+    */
+   public void testGetDisplayableViewName(
+   ) throws Exception
+   {
+		DataDescriptor descriptor1;
+		
+      // First create simple instance of the data descriptor, since we should
+      // be able to get displayable view name this way just fine
+      descriptor1 = new TestDataObjectDataDescriptor();
+      
+		assertEquals("Data displayable view name doesn't match",
+					    TestDataObject.TestDataObjectDataDescriptor.TEST_DATA_TYPE_NAME,
+						 descriptor1.getDisplayableViewName());
+
+      // Data descriptors should be really accessed using DataDescriptorManager
+      // which properly initializes them, we shouldbe able to still get desired 
+      // data type this way just fine
+      descriptor1 = DataDescriptorManager.getInstance(TestDataObjectDataDescriptor.class);
+      
+      assertNotNull("DataDescriptorManager must always be able to create instance"
+                    + " of data descriptor", descriptor1);
+      
+      // When properly initialized by DataDescriptorManager we should be able 
+      // to get the data type
+		assertEquals("Data displayable view name doesn't match",
+					    TestDataObject.TestDataObjectDataDescriptor.TEST_DATA_TYPE_NAME,
+						 descriptor1.getDisplayableViewName());
+      
+      // Another way how to register the descriptor with DataDescriptorManager
+      // is to create data type that uses this descriptor,  we should be able 
+      // to get the data type
+		TestDataObject data = new TestDataObject();
+		DataDescriptor descriptor2;
+
+		assertEquals("Data descriptor doesnt match the expected class",
+					    TestDataObject.TestDataObjectDataDescriptor.class,
+						 data.getDataDescriptorClass());
+      
+		descriptor2 = data.getDataDescriptor();
+      
+      // This also implies that the data type is of course the same
+		assertTrue("Data descriptor instance is shared between all instances of class",
+					  descriptor1 == descriptor2);      
+   }
+
+   /**
+    * Test getViewName method 
+    * 
+    * @throws Exception - and error has occurred  
+    */
+   public void testGetViewName(
+   ) throws Exception
+   {
+		DataDescriptor descriptor1;
+		
+      // First create simple instance of the data descriptor, since we should
+      // be able to get displayable view name this way just fine
+      descriptor1 = new TestDataObjectDataDescriptor();
+      
+		assertEquals("Data view name doesn't match",
+					    TestDataObject.TestDataObjectDataDescriptor.TEST_TYPE_VIEW,
+						 descriptor1.getViewName());
+
+      // Data descriptors should be really accessed using DataDescriptorManager
+      // which properly initializes them, we shouldbe able to still get desired 
+      // data type this way just fine
+      descriptor1 = DataDescriptorManager.getInstance(TestDataObjectDataDescriptor.class);
+      
+      assertNotNull("DataDescriptorManager must always be able to create instance"
+                    + " of data descriptor", descriptor1);
+      
+      // When properly initialized by DataDescriptorManager we should be able 
+      // to get the data type
+		assertEquals("Data view name doesn't match",
+					    TestDataObject.TestDataObjectDataDescriptor.TEST_TYPE_VIEW,
+						 descriptor1.getViewName());
+      
+      // Another way how to register the descriptor with DataDescriptorManager
+      // is to create data type that uses this descriptor,  we should be able 
+      // to get the data type
+		TestDataObject data = new TestDataObject();
+		DataDescriptor descriptor2;
+
+		assertEquals("Data descriptor doesnt match the expected class",
+					    TestDataObject.TestDataObjectDataDescriptor.class,
+						 data.getDataDescriptorClass());
+      
+		descriptor2 = data.getDataDescriptor();
+      
+      // This also implies that the data type is of course the same
+		assertTrue("Data descriptor instance is shared between all instances of class",
+					  descriptor1 == descriptor2);      
+   }
+
+   /**
+    * Test getFieldMaxLength method 
+    * 
+    * @throws Exception - and error has occurred  
+    */
+   public void testGetFieldMaxLength(
+   ) throws Exception
+   {
+		DataDescriptor descriptor1;
+		
+      // First create simple instance of the data descriptor, since we should
+      // be able to get displayable view name this way just fine
+      descriptor1 = new TestDataObjectDataDescriptor();
+
+// TODO: We really need method descriptor1.getFields() 
+      
+      assertNotNull("Test data object has to have come fields defined",
+                    TestDataObjectDataDescriptor.TestDataFields.values());
+      
+      assertEquals("Test data object has to have expected set of fields", 4,
+                    TestDataObjectDataDescriptor.TestDataFields.values().length);
+      
+      for (Enum field : TestDataObjectDataDescriptor.TestDataFields.values())
+      {
+         assertEquals("When created directly all fields have 0 max length",
+                      (Integer)0, descriptor1.getFieldMaxLength(field));
+      }
+      
+      // Data descriptors should be really accessed using DataDescriptorManager
+      // which properly initializes them, we shouldbe able to still get desired 
+      // data type this way just fine
+      descriptor1 = DataDescriptorManager.getInstance(TestDataObjectDataDescriptor.class);
+      
+      assertNotNull("DataDescriptorManager must always be able to create instance"
+                    + " of data descriptor", descriptor1);
+      
+      
+      
+      // When properly initialized by DataDescriptorManager we should be able 
+      // to get the data type
+		assertEquals("Data type doesn't match",
+					    TestDataObject.TestDataObjectDataDescriptor.TEST_TYPE_VIEW,
+						 descriptor1.getViewName());
+      
+      // Another way how to register the descriptor with DataDescriptorManager
+      // is to create data type that uses this descriptor,  we should be able 
+      // to get the data type
+		TestDataObject data = new TestDataObject();
+		DataDescriptor descriptor2;
+
+		assertEquals("Data descriptor doesnt match the expected class",
+					    TestDataObject.TestDataObjectDataDescriptor.class,
+						 data.getDataDescriptorClass());
+      
+		descriptor2 = data.getDataDescriptor();
+      
+      // This also implies that the data type is of course the same
+		assertTrue("Data descriptor instance is shared between all instances of class",
+					  descriptor1 == descriptor2);      
+   }
 }
